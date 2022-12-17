e86242a129fa403fcfe504f00333b995d69eb2e4
ZeusFtrOfc <77143853+ZeusFtrOfc@users.noreply.github.com>
2022-12-17 12:39:29 +0700

    Add files via upload

--- utils/message.js
+++ utils/message.js
@@ -4,149 +4,85 @@
  */
 exports.help = (prefix) => {
     return `
-*┏━──✪[ _INFORMATION_ ]✪──╮*
+*┏━──✪[ _Bot Information_ ]✪──╮*
 *┃➥ AUTHOR : AXV*
 *┃➥ WEBSITE : https://axvdigital.com*
+*┃➥ YOUTUBE : https://www.youtube.com/@axvdigital3071
 *┃➥ _HAVE FUN BREE :)..._*
 *┗━───────────────╯*
 
-*┏━──✪[ _MENU_ ]✪──╮*
-*┃➥ ${prefix}owner*
-*┃➥ ${prefix}brainly*
-*┃➥ ${prefix}robotguru*
-*┃➥ ${prefix}listsurah*
-*┃➥ ${prefix}alquran*
-*┃➥ ${prefix}alquranaudio*
-*┃➥ ${prefix}asmaulhusna*
-*┃➥ ${prefix}kisahnabi*
-*┃➥ ${prefix}jadwalsholat*
-*┃➥ ${prefix}ytplay*
-*┃➥ ${prefix}ytsearch*
-*┃➥ ${prefix}ytmp3*
-*┃➥ ${prefix}ytmp4*
-*┃➥ ${prefix}telesticker*
-*┃➥ ${prefix}tiktoknowm*
-*┃➥ ${prefix}tiktokmusic*
-*┃➥ ${prefix}spotify*
-*┃➥ ${prefix}spotifysearch*
-*┃➥ ${prefix}jooxplay*
-*┃➥ ${prefix}igdl*
-*┃➥ ${prefix}igdl2*
-*┃➥ ${prefix}twtdl*
-*┃➥ ${prefix}fbdl*
-*┃➥ ${prefix}zippyshare*
-*┃➥ ${prefix}pinterest*
-*┃➥ ${prefix}pinterest2*
-*┃➥ ${prefix}pinterestdl*
-*┃➥ ${prefix}pixiv*
-*┃➥ ${prefix}pixivdl*
-*┃➥ ${prefix}character*
-*┃➥ ${prefix}manga*
-*┃➥ ${prefix}anime*
-*┃➥ ${prefix}wait*
-*┃➥ ${prefix}kusonime*
-*┃➥ ${prefix}kusonimesearch*
-*┃➥ ${prefix}otakudesu*
-*┃➥ ${prefix}otakudesusearch*
-*┃➥ ${prefix}1977*
-*┃➥ ${prefix}aden*
-*┃➥ ${prefix}brannan*
-*┃➥ ${prefix}brooklyn*
-*┃➥ ${prefix}clarendon*
-*┃➥ ${prefix}gingham*
-*┃➥ ${prefix}hudson*
-*┃➥ ${prefix}inkwell*
-*┃➥ ${prefix}earlybird*
-*┃➥ ${prefix}kelvin*
-*┃➥ ${prefix}lark*
-*┃➥ ${prefix}lofi*
-*┃➥ ${prefix}maven*
-*┃➥ ${prefix}mayfair*
-*┃➥ ${prefix}moon*
-*┃➥ ${prefix}nashville*
-*┃➥ ${prefix}perpetua*
-*┃➥ ${prefix}reyes*
-*┃➥ ${prefix}rise*
-*┃➥ ${prefix}slumber*
-*┃➥ ${prefix}stinson*
-*┃➥ ${prefix}toaster*
-*┃➥ ${prefix}valencia*
-*┃➥ ${prefix}walden*
-*┃➥ ${prefix}willow*
-*┃➥ ${prefix}xpro2*
-*┃➥ ${prefix}pencil*
-*┃➥ ${prefix}art*
-*┃➥ ${prefix}bts*
-*┃➥ ${prefix}exo*
-*┃➥ ${prefix}elf*
-*┃➥ ${prefix}loli*
-*┃➥ ${prefix}neko*
-*┃➥ ${prefix}waifu*
-*┃➥ ${prefix}shota*
-*┃➥ ${prefix}husbu*
-*┃➥ ${prefix}sagiri*
-*┃➥ ${prefix}shinobu*
-*┃➥ ${prefix}megumin*
-*┃➥ ${prefix}wallnime*
-*┃➥ ${prefix}chiisaihentai*
-*┃➥ ${prefix}trap*
-*┃➥ ${prefix}blowjob*
-*┃➥ ${prefix}yaoi*
-*┃➥ ${prefix}ecchi*
-*┃➥ ${prefix}hentai*
-*┃➥ ${prefix}ahegao*
-*┃➥ ${prefix}hololewd*
-*┃➥ ${prefix}sideoppai*
-*┃➥ ${prefix}animefeets*
-*┃➥ ${prefix}animebooty*
-*┃➥ ${prefix}animethighss*
-*┃➥ ${prefix}hentaiparadise*
-*┃➥ ${prefix}animearmpits*
-*┃➥ ${prefix}hentaifemdom*
-*┃➥ ${prefix}lewdanimegirls*
-*┃➥ ${prefix}biganimetiddies*
-*┃➥ ${prefix}animebellybutton*
-*┃➥ ${prefix}hentai4everyone*
-*┃➥ ${prefix}bj*
-*┃➥ ${prefix}ero*
-*┃➥ ${prefix}cum*
-*┃➥ ${prefix}feet*
-*┃➥ ${prefix}yuri*
-*┃➥ ${prefix}trap*
-*┃➥ ${prefix}lewd*
-*┃➥ ${prefix}feed*
-*┃➥ ${prefix}eron*
-*┃➥ ${prefix}solo*
-*┃➥ ${prefix}gasm*
-*┃➥ ${prefix}poke*
-*┃➥ ${prefix}anal*
-*┃➥ ${prefix}holo*
-*┃➥ ${prefix}kuni*
-*┃➥ ${prefix}kiss*
-*┃➥ ${prefix}erok*
-*┃➥ ${prefix}smug*
-*┃➥ ${prefix}baka*
-*┃➥ ${prefix}feetg*
-*┃➥ ${prefix}lewdk*
-*┃➥ ${prefix}waifu*
-*┃➥ ${prefix}pussy*
-*┃➥ ${prefix}femdom*
-*┃➥ ${prefix}cuddle*
-*┃➥ ${prefix}hentai*
-*┃➥ ${prefix}eroyuri*
-*┃➥ ${prefix}cum_jpg*
-*┃➥ ${prefix}blowjob*
-*┃➥ ${prefix}erofeet*
-*┃➥ ${prefix}holoero*
-*┃➥ ${prefix}classic*
-*┃➥ ${prefix}erokemo*
-*┃➥ ${prefix}fox_girl*
-*┃➥ ${prefix}futanari*
-*┃➥ ${prefix}lewdkemo*
-*┃➥ ${prefix}wallpaper*
-*┃➥ ${prefix}pussy_jpg*
-*┃➥ ${prefix}kemonomimi*
-*┃➥ ${prefix}nsfw_avatar*
+*┏━──✪[ _Ephoto 360_ ]✪──╮*
+*┃➥ ${prefix}wetglass*
+*┃➥ ${prefix}multicolor3d*
+*┃➥ ${prefix}watercolor*
+*┃➥ ${prefix}luxurygold*
+*┃➥ ${prefix}galaxywallpaper*
+*┃➥ ${prefix}lighttext*
+*┃➥ ${prefix}beautifulflower*
+*┃➥ ${prefix}puppycute*
+*┃➥ ${prefix}royaltext*
+*┃➥ ${prefix}heartshaped*
+*┃➥ ${prefix}birthdaycake*
+*┃➥ ${prefix}galaxystyle*
+*┃➥ ${prefix}hologram3d*
+*┃➥ ${prefix}greenneon*
+*┃➥ ${prefix}glossychrome*
+*┃➥ ${prefix}greenbush*
+*┃➥ ${prefix}metallogo*
+*┃➥ ${prefix}noeltext*
+*┃➥ ${prefix}glittergold*
+*┃➥ ${prefix}textcake*
+*┃➥ ${prefix}starsnight*
+*┃➥ ${prefix}wooden3d*
+*┃➥ ${prefix}textbyname*
+*┃➥ ${prefix}writegalacy*
+*┃➥ ${prefix}galaxybat*
+*┃➥ ${prefix}snow3d*
+*┃➥ ${prefix}birthdayday*
+*┃➥ ${prefix}goldplaybutton*
+*┃➥ ${prefix}silverplaybutton*
+*┃➥ ${prefix}freefire*
+*┃➥ ${prefix}mlwall*
+*┃➥ ${prefix}fpslogo*
+*┃➥ ${prefix}lolbanner*
+*┃➥ ${prefix}valorantbanner*
+*┃➥ ${prefix}realvintage*
+*┃➥ ${prefix}anonymhacker*
+*┃➥ ${prefix}cartoongravity*
+*┗━───────────────╯*
+
+*┏━──✪[ _Photo Oxy_ ]✪──╮*
+*┃➥ ${prefix}shadow*
+*┃➥ ${prefix}cup*
+*┃➥ ${prefix}cup1*
+*┃➥ ${prefix}romance*
+*┃➥ ${prefix}smoke*
+*┃➥ ${prefix}burnpaper*
+*┃➥ ${prefix}lovemessage*
+*┃➥ ${prefix}undergrass*
+*┃➥ ${prefix}love*
+*┃➥ ${prefix}coffe*
+*┃➥ ${prefix}woodheart*
+*┃➥ ${prefix}woodenboard*
+*┃➥ ${prefix}summer3d*
+*┃➥ ${prefix}wolfmetal*
+*┃➥ ${prefix}nature3d*
+*┃➥ ${prefix}underwater*
+*┃➥ ${prefix}golderrose*
+*┃➥ ${prefix}summernature*
+*┃➥ ${prefix}letterleaves*
+*┃➥ ${prefix}glowingneon*
+*┃➥ ${prefix}fallleaves*
+*┃➥ ${prefix}flamming*
+*┃➥ ${prefix}harrypotter*
+*┃➥ ${prefix}carvedwood*
+*┃➥ ${prefix}tiktok*
+*┃➥ ${prefix}arcade8bit*
+*┃➥ ${prefix}battlefield4*
+*┃➥ ${prefix}pubg*
+*┗━───────────────╯*
+
+*┏━──✪[ _Textprome_ ]✪──╮*
 *┃➥ ${prefix}blackpink*
 *┃➥ ${prefix}neon*
 *┃➥ ${prefix}greenneon*
@@ -189,63 +125,277 @@ exports.help = (prefix) => {
 *┃➥ ${prefix}wolflogo*
 *┃➥ ${prefix}steel3d*
 *┃➥ ${prefix}wallgravity*
-*┃➥ ${prefix}shadow*
-*┃➥ ${prefix}cup*
-*┃➥ ${prefix}cup1*
-*┃➥ ${prefix}romance*
-*┃➥ ${prefix}smoke*
-*┃➥ ${prefix}burnpaper*
-*┃➥ ${prefix}lovemessage*
-*┃➥ ${prefix}undergrass*
-*┃➥ ${prefix}love*
-*┃➥ ${prefix}coffe*
-*┃➥ ${prefix}woodheart*
-*┃➥ ${prefix}woodenboard*
-*┃➥ ${prefix}summer3d*
-*┃➥ ${prefix}wolfmetal*
-*┃➥ ${prefix}nature3d*
-*┃➥ ${prefix}underwater*
-*┃➥ ${prefix}golderrose*
-*┃➥ ${prefix}summernature*
-*┃➥ ${prefix}letterleaves*
-*┃➥ ${prefix}glowingneon*
-*┃➥ ${prefix}fallleaves*
-*┃➥ ${prefix}flamming*
-*┃➥ ${prefix}harrypotter*
-*┃➥ ${prefix}carvedwood*
-*┃➥ ${prefix}tiktok*
-*┃➥ ${prefix}arcade8bit*
-*┃➥ ${prefix}battlefield4*
-*┃➥ ${prefix}pubg*
-*┃➥ ${prefix}wetglass*
-*┃➥ ${prefix}multicolor3d*
-*┃➥ ${prefix}watercolor*
-*┃➥ ${prefix}luxurygold*
-*┃➥ ${prefix}galaxywallpaper*
-*┃➥ ${prefix}lighttext*
-*┃➥ ${prefix}beautifulflower*
-*┃➥ ${prefix}puppycute*
-*┃➥ ${prefix}royaltext*
-*┃➥ ${prefix}heartshaped*
-*┃➥ ${prefix}birthdaycake*
-*┃➥ ${prefix}galaxystyle*
-*┃➥ ${prefix}hologram3d*
-*┃➥ ${prefix}greenneon*
-*┃➥ ${prefix}glossychrome*
-*┃➥ ${prefix}greenbush*
-*┃➥ ${prefix}metallogo*
-*┃➥ ${prefix}noeltext*
-*┃➥ ${prefix}glittergold*
-*┃➥ ${prefix}textcake*
-*┃➥ ${prefix}starsnight*
-*┃➥ ${prefix}wooden3d*
-*┃➥ ${prefix}textbyname*
-*┃➥ ${prefix}writegalacy*
-*┃➥ ${prefix}galaxybat*
-*┃➥ ${prefix}snow3d*
-*┃➥ ${prefix}birthdayday*
-*┃➥ ${prefix}goldplaybutton*
-*┃➥ ${prefix}silverplaybutton*
-*┃➥ ${prefix}freefire*
-*┗━───────────────╯*`
+*┗━───────────────╯*
+
+*┏━──✪[ _Random Image_ ]✪──╮*
+*┃➥ ${prefix}art*
+*┃➥ ${prefix}bts*
+*┃➥ ${prefix}exo*
+*┃➥ ${prefix}elf*
+*┃➥ ${prefix}loli*
+*┃➥ ${prefix}neko*
+*┃➥ ${prefix}waifu*
+*┃➥ ${prefix}shota*
+*┃➥ ${prefix}husbu*
+*┃➥ ${prefix}sagiri*
+*┃➥ ${prefix}shinobu*
+*┃➥ ${prefix}megumin*
+*┃➥ ${prefix}wallnime*
+*┃➥ ${prefix}quotesimage*
+*┃➥ ${prefix}chiisaihentai*
+*┃➥ ${prefix}trap*
+*┃➥ ${prefix}blowjob*
+*┃➥ ${prefix}yaoi*
+*┃➥ ${prefix}ecchi*
+*┃➥ ${prefix}ahegao*
+*┃➥ ${prefix}hololewd*
+*┃➥ ${prefix}sideoppai*
+*┃➥ ${prefix}animefeets*
+*┃➥ ${prefix}animebooty*
+*┃➥ ${prefix}animethighss*
+*┃➥ ${prefix}hentaiparadise*
+*┃➥ ${prefix}animearmpits*
+*┃➥ ${prefix}hentaifemdom*
+*┃➥ ${prefix}lewdanimegirls*
+*┃➥ ${prefix}biganimetiddies*
+*┃➥ ${prefix}animebellybutton*
+*┃➥ ${prefix}hentai4everyone*
+*┃➥ ${prefix}hentai*
+*┃➥ ${prefix}bj*
+*┃➥ ${prefix}ero*
+*┃➥ ${prefix}cum*
+*┃➥ ${prefix}feet*
+*┃➥ ${prefix}yuri*
+*┃➥ ${prefix}trap*
+*┃➥ ${prefix}lewd*
+*┃➥ ${prefix}feed*
+*┃➥ ${prefix}eron*
+*┃➥ ${prefix}solo*
+*┃➥ ${prefix}gasm*
+*┃➥ ${prefix}poke*
+*┃➥ ${prefix}anal*
+*┃➥ ${prefix}holo*
+*┃➥ ${prefix}tits*
+*┃➥ ${prefix}kuni*
+*┃➥ ${prefix}kiss*
+*┃➥ ${prefix}erok*
+*┃➥ ${prefix}smug*
+*┃➥ ${prefix}baka*
+*┃➥ ${prefix}solog*
+*┃➥ ${prefix}feetg*
+*┃➥ ${prefix}lewdk*
+*┃➥ ${prefix}waifu*
+*┃➥ ${prefix}pussy*
+*┃➥ ${prefix}femdom*
+*┃➥ ${prefix}cuddle*
+*┃➥ ${prefix}hentai*
+*┃➥ ${prefix}eroyuri*
+*┃➥ ${prefix}cum_jpg*
+*┃➥ ${prefix}blowjob*
+*┃➥ ${prefix}erofeet*
+*┃➥ ${prefix}holoero*
+*┃➥ ${prefix}classic*
+*┃➥ ${prefix}erokemo*
+*┃➥ ${prefix}fox_girl*
+*┃➥ ${prefix}futanari*
+*┃➥ ${prefix}lewdkemo*
+*┃➥ ${prefix}wallpaper*
+*┃➥ ${prefix}pussy_jpg*
+*┃➥ ${prefix}kemonomimi*
+*┃➥ ${prefix}nsfw_avatar*
+*┗━───────────────╯*
+
+*┏━──✪[ _Image Creator_ ]✪──╮*
+*┃➥ ${prefix}ramadhan*
+*┃➥ ${prefix}amongus*
+*┃➥ ${prefix}badboy*
+*┃➥ ${prefix}badgirl*
+*┃➥ ${prefix}bucinserti*
+*┃➥ ${prefix}carbon*
+*┃➥ ${prefix}tweettrump*
+*┃➥ ${prefix}fuckboy*
+*┃➥ ${prefix}fuckgirl*
+*┃➥ ${prefix}goodboy*
+*┃➥ ${prefix}goodgirl*
+*┃➥ ${prefix}idulfitri*
+*┃➥ ${prefix}nulis*
+*┃➥ ${prefix}qrcode*
+*┃➥ ${prefix}quotemaker*
+*┃➥ ${prefix}toloserti*
+*┗━───────────────╯*
+
+*┏━──✪[ _Islami_ ]✪──╮*
+*┃➥ ${prefix}asmaulhusna*
+*┃➥ ${prefix}jadwalshola*
+*┃➥ ${prefix}kisahnabi*
+*┃➥ ${prefix}alquranaudio*
+*┃➥ ${prefix}alquran*
+*┃➥ ${prefix}listsurah*
+*┗━───────────────╯*
+
+*┏━──✪[ _Downloader_ ]✪──╮*
+*┃➥ ${prefix}ytplay*
+*┃➥ ${prefix}ytsearch*
+*┃➥ ${prefix}play*
+*┃➥ ${prefix}ytmp3*
+*┃➥ ${prefix}ytmp4*
+*┃➥ ${prefix}telesticker*
+*┃➥ ${prefix}tiktokwm*
+*┃➥ ${prefix}tiktoknowm*
+*┃➥ ${prefix}tiktokmusic*
+*┃➥ ${prefix}spotify*
+*┃➥ ${prefix}spotifysearch*
+*┃➥ ${prefix}jooxplay*
+*┃➥ ${prefix}igdl*
+*┃➥ ${prefix}igdl2*
+*┃➥ ${prefix}twtdl*
+*┃➥ ${prefix}fbdl*
+*┃➥ ${prefix}zippyshare*
+*┃➥ ${prefix}pinterest*
+*┃➥ ${prefix}pinterest2*
+*┃➥ ${prefix}pinterestdl*
+*┃➥ ${prefix}pixiv*
+*┃➥ ${prefix}pixivdl*
+*┗━───────────────╯*
+
+*┏━──✪[ _AniManga_ ]✪──╮*
+*┃➥ ${prefix}character*
+*┃➥ ${prefix}manga*
+*┃➥ ${prefix}anime*
+*┃➥ ${prefix}wait*
+*┃➥ ${prefix}kusonime*
+*┃➥ ${prefix}kusonimesearch*
+*┃➥ ${prefix}otakudesu*
+*┃➥ ${prefix}otakudesusearch*
+*┗━───────────────╯*
+
+*┏━──✪[ _Information_ ]✪──╮*
+*┃➥ ${prefix}kbbi*
+*┃➥ ${prefix}brainly*
+*┃➥ ${prefix}robotguru*
+*┃➥ ${prefix}jarak*
+*┃➥ ${prefix}cuaca*
+*┃➥ ${prefix}urbandictionary*
+*┃➥ ${prefix}heroml*
+*┃➥ ${prefix}mlstalk*
+*┃➥ ${prefix}genshin*
+*┃➥ ${prefix}qrreader*
+*┃➥ ${prefix}wikipedia*
+*┃➥ ${prefix}translate*
+*┃➥ ${prefix}jadwaltv*
+*┃➥ ${prefix}jadwaltvnow*
+*┃➥ ${prefix}newsinfo*
+*┃➥ ${prefix}cnnindonesia*
+*┃➥ ${prefix}cnnnasional*
+*┃➥ ${prefix}cnninternasional*
+*┃➥ ${prefix}infogempa*
+*┃➥ ${prefix}covidindo*
+*┃➥ ${prefix}covidglobal*
+*┃➥ ${prefix}kodepos*
+*┃➥ ${prefix}jadwalbola*
+*┃➥ ${prefix}indbeasiswa*
+*┃➥ ${prefix}hoax*
+*┃➥ ${prefix}nsfwcheck*
+*┃➥ ${prefix}ocr*
+*┗━───────────────╯*
+
+*┏━──✪[ _Movie & Story_ ]✪──╮*
+*┃➥ ${prefix}lk21*
+*┃➥ ${prefix}drakorongoing*
+*┃➥ ${prefix}wattpad*
+*┃➥ ${prefix}wattpadsearch*
+*┃➥ ${prefix}cerpen*
+*┃➥ ${prefix}ceritahoror*
+*┗━───────────────╯*
+
+*┏━──✪[ _Searching_ ]✪──╮*
+*┃➥ ${prefix}gimage*
+*┃➥ ${prefix}konachan*
+*┃➥ ${prefix}wallpapersearch*
+*┃➥ ${prefix}gimage2*
+*┃➥ ${prefix}wallpapersearch2*
+*┃➥ ${prefix}playstore*
+*┃➥ ${prefix}shopee*
+*┃➥ ${prefix}google*
+*┃➥ ${prefix}chord*
+*┃➥ ${prefix}lirik*
+*┗━───────────────╯*
+
+*┏━──✪[ _Random Text_ ]✪──╮*
+*┃➥ ${prefix}quotes*
+*┃➥ ${prefix}quotesanime*
+*┃➥ ${prefix}quotesdilan*
+*┃➥ ${prefix}faktaunik*
+*┃➥ ${prefix}katabijak*
+*┃➥ ${prefix}pantun*
+*┃➥ ${prefix}bucin*
+*┃➥ ${prefix}randomnama*
+*┗━───────────────╯*
+
+*┏━──✪[ _Primbon_ ]✪──╮*
+*┃➥ ${prefix}artinama*
+*┃➥ ${prefix}jodoh*
+*┃➥ ${prefix}weton*
+*┃➥ ${prefix}jadian*
+*┃➥ ${prefix}tebakumur*
+*┗━───────────────╯*
+
+*┏━──✪[ _Stalk_ ]✪──╮*
+*┃➥ ${prefix}stalkig*
+*┃➥ ${prefix}stalkgithub*
+*┃➥ ${prefix}stalktwitter*
+*┃➥ ${prefix}stalktiktok*
+*┗━───────────────╯*
+
+*┏━──✪[ _Filter_ ]✪──╮*
+*┃➥ ${prefix}1977*
+*┃➥ ${prefix}aden*
+*┃➥ ${prefix}brannan*
+*┃➥ ${prefix}brooklyn*
+*┃➥ ${prefix}clarendon*
+*┃➥ ${prefix}gingham*
+*┃➥ ${prefix}hudson*
+*┃➥ ${prefix}inkwell*
+*┃➥ ${prefix}earlybird*
+*┃➥ ${prefix}kelvin*
+*┃➥ ${prefix}lark*
+*┃➥ ${prefix}lofi*
+*┃➥ ${prefix}maven*
+*┃➥ ${prefix}mayfair*
+*┃➥ ${prefix}moon*
+*┃➥ ${prefix}nashville*
+*┃➥ ${prefix}perpetua*
+*┃➥ ${prefix}reyes*
+*┃➥ ${prefix}rise*
+*┃➥ ${prefix}slumber*
+*┃➥ ${prefix}stinson*
+*┃➥ ${prefix}toaster*
+*┃➥ ${prefix}valencia*
+*┃➥ ${prefix}walden*
+*┃➥ ${prefix}willow*
+*┃➥ ${prefix}xpro2*
+*┃➥ ${prefix}pencil*
+*┃➥ ${prefix}quotemaker3*
+*┃➥ ${prefix}roundsticker*
+*┃➥ ${prefix}affect*
+*┃➥ ${prefix}beautiful*
+*┃➥ ${prefix}facepalm*
+*┃➥ ${prefix}hitler*
+*┃➥ ${prefix}jail*
+*┃➥ ${prefix}jokeOverHead*
+*┃➥ ${prefix}rainbow*
+*┃➥ ${prefix}rip*
+*┃➥ ${prefix}sepia*
+*┃➥ ${prefix}trash*
+*┃➥ ${prefix}wanted*
+*┗━───────────────╯*
+
+*┏━──✪[ _Other_ ]✪──╮*
+*┃➥ ${prefix}ssweb*
+*┃➥ ${prefix}ssweb2*
+*┃➥ ${prefix}shortlink*
+*┃➥ ${prefix}sticker*
+*┃➥ ${prefix}stickerwm*
+*┗━───────────────╯*
+`
 }
